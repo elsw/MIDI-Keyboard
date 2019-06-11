86951ba0f691d04bf633b5214d01285ecd1b464d
Stephen Withers <SteveWithers3@gmail.com>
2019-06-11 23:41:28 +0100

    bug fixing

--- keyboard_main.py
+++ keyboard_main.py
@@ -1,40 +1,45 @@
 import mido
 import pigpio
+import time
 
 TOTAL_KEYS = 61
 KEYS_ROWS = 8
 KEYS_COLUMNS = 8
 
-GPIO_ROWS = [24,10,9,25,11,8,7,5] # Outputs
-GPIO_COLUMNS = [6,12,13,19,16,26,20,21] #Inputs
+GPIO_COLUMNS = [24,10,9,25,11,8,7,5] # Outputs
+GPIO_ROWS = [6,12,13,19,16,26,20,21] #Inputs
 
 KEY_TO_MIDI_NOTE = [\
-    list(range(27,35)),\
-    list(range(35,43)),\
-    list(range(43,51)),\
-    list(range(51,58)),\
-    list(range(58,65)),\
-    list(range(65,73)),\
-    list(range(73,81)),\
-    list(range(81,89)) ]
+    list(range(36,44)),\
+    list(range(44,52)),\
+    list(range(52,60)),\
+    list(range(60,68)),\
+    list(range(68,76)),\
+    list(range(76,84)),\
+    list(range(84,92)),\
+    list(range(92,100)) ]
 
-KEY_STATES = [ [1] * KEYS_COLUMNS ] * KEYS_ROWS
+KEY_STATES = dict()
+#fill dictinoary with states for each midi note
+for i in range(0, KEYS_ROWS):
+    for j in range(0, KEYS_COLUMNS):
+        KEY_STATES[KEY_TO_MIDI_NOTE[j][i]] = 1
 
 # 0 (low) = ON , 1 (high) = OFF
-KEY_STATE = ['note_on','note_off']
+KEY_VALUE = ['note_on','note_off']
 
 pi = pigpio.pi()
 output = mido.open_output('f_midi')
 
 #setup GPIO
-for LED in GPIO_COLUMNS:
-    print("Setup input GPIO " + str(LED))
-    pi.set_mode(LED, pigpio.INPUT)
-    pi.set_pull_up_down(LED, pigpio.PUD_UP)
+for i in GPIO_COLUMNS:
+    print("Setup input GPIO ",i)
+    pi.set_mode(i, pigpio.INPUT)
+    pi.set_pull_up_down(i, pigpio.PUD_UP)
 
-for LED in GPIO_ROWS:
-    print("Setup output GPIO " + str(LED_)
-    pi.set_mode(LED, pigpio.OUTPUT)
+for i in GPIO_ROWS:
+    print("Setup output GPIO ",i)
+    pi.set_mode(i, pigpio.OUTPUT)
 
 while True:
     for i in range(0 , KEYS_ROWS):
@@ -45,10 +50,11 @@ while True:
         for j in range(0 , KEYS_COLUMNS):
             #read column
             state = pi.read(GPIO_COLUMNS[j])
-            if state != KEY_STATES[i][j]:
-                output.send(mido.Message(KEY_STATE[state], note = KEY_TO_MIDI_NOTE[i][j] , velocity = 127))
-                KEY_STATES[i][j] = state
-                print("Key " + str(KEY_TO_MIDI_NOTE[i][j]) + " " + KEY_STATE[state])
+            midi_note = KEY_TO_MIDI_NOTE[j][i]
+            if KEY_STATES[midi_note] != state:
+                output.send(mido.Message(KEY_VALUE[state], note = midi_note , velocity = 127))
+                KEY_STATES[midi_note] = state
+                #print("Key ",midi_note,KEY_VALUE[state])
                 
     pi.write(GPIO_ROWS[KEYS_ROWS-1],1)
             
